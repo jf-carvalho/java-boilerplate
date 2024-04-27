7e1f74df35614ded4f717ed8778b16782061c8d4
João Carvalho <joao.carvalho@dentrodahistoria.com.br>
2024-04-27 20:09:07 -0300

    creating role persistence entity and migration

---
+++ src/main/java/com/app/infrastructure/persistence/entity/Role.java
@@ -0,0 +1,47 @@
+package com.app.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<User>();
+
+    public Role() {
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
